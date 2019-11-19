f1852af268942777bec28b703ecf9bc0ab7b2690
Courtney Campany <courtneycampany@gmail.com>
2019-11-19 10:17:59 -0500

    start project with vectors and weather

---
+++ mammal_dat.R
@@ -0,0 +1,26 @@
+library(neonUtilities)
+
+stackByTable("data_downloads/NEON_count-ticks.zip",
+             savepath="raw_data/tick_data")
+
+stackByTable("data_downloads/NEON_count-mosquitoes.zip",
+             savepath="raw_data/mosquito_data")
+
+stackByTable("data_downloads/NEON_pathogens-mosquito.zip",
+             savepath="raw_data/mosq_path")
+
+stackByTable("data_downloads/NEON_pathogens-tick.zip",
+             savepath="raw_data/tick_path")
+
+#stack weather
+stackByTable("data_downloads/NEON_temp-air-triple.zip",
+             savepath="weather_raw/temp")
+
+stackByTable("data_downloads/NEON_precipitation.zip",
+             savepath="weather_raw/rain")
+
+stackByTable("data_downloads/NEON_weather-stats.zip",
+             savepath="weather_raw/weather")
+
+stackByTable("data_downloads/NEON_rel-humidity.zip",
+             savepath="weather_raw/humidity")
