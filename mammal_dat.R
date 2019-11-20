e8a4f6dcbdaf6d836e3c41589b1427319b1405d8
Courtney Campany <courtneycampany@gmail.com>
2019-11-20 08:32:45 -0500

    still strugs with weather data

--- mammal_dat.R
+++ mammal_dat.R
@@ -1,7 +1,7 @@
 library(neonUtilities)
 
-stackByTable("data_downloads/NEON_count-ticks.zip",
-             savepath="raw_data/tick_data")
+# stackByTable("data_downloads/NEON_count-ticks.zip",
+#              savepath="raw_data/tick_data")
 
 stackByTable("data_downloads/NEON_count-mosquitoes.zip",
              savepath="raw_data/mosquito_data")
@@ -9,18 +9,18 @@ stackByTable("data_downloads/NEON_count-mosquitoes.zip",
 stackByTable("data_downloads/NEON_pathogens-mosquito.zip",
              savepath="raw_data/mosq_path")
 
-stackByTable("data_downloads/NEON_pathogens-tick.zip",
-             savepath="raw_data/tick_path")
+# stackByTable("data_downloads/NEON_pathogens-tick.zip",
+#              savepath="raw_data/tick_path")
 
 #stack weather
-stackByTable("data_downloads/NEON_temp-air-triple.zip",
-             savepath="weather_raw/temp")
-
-stackByTable("data_downloads/NEON_precipitation.zip",
-             savepath="weather_raw/rain")
+# stackByTable("data_downloads/NEON_temp-air-triple.zip",
+#              savepath="weather_raw/temp")
+# 
+# stackByTable("data_downloads/NEON_precipitation.zip",
+#              savepath="weather_raw/rain")
 
 stackByTable("data_downloads/NEON_weather-stats.zip",
-             savepath="weather_raw/weather")
+             savepath="raw_data/weather")
 
-stackByTable("data_downloads/NEON_rel-humidity.zip",
-             savepath="weather_raw/humidity")
+# stackByTable("data_downloads/NEON_rel-humidity.zip",
+#              savepath="weather_raw/humidity")
